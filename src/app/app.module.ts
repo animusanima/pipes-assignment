6cda4ef4e9a5259f8fc911c8c37ad6aeae381b6b
animusanima <molockokannzocko@gmail.com>
2024-05-22 18:00:46 +0200

    Final state of the pipe-assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
+
+import {AppComponent} from './app.component';
+import {ReversePipe} from './reverse.pipe';
+import {OrderByPipe} from './order-by.pipe';
+import {SortPipe} from "./sort.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReversePipe,
+    OrderByPipe,
+    SortPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
